21eebc8abe8d8cee6622e994a00e95e17b37ae9d
Azeem <azeemahmed101@gmail.com>
2020-05-04 12:01:56 +0200

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,6 @@ unsorted_list = [6, 2, 8, 1, 7, 3, 4]
 
 def bubble_sort(array)
   k = 0
-  n = 0
   (array.length - 1).times do
     k += 1
     i = 0
@@ -15,31 +14,27 @@ def bubble_sort(array)
       elsif (array[i] <=> array[i + 1]) == -1
         i += 1
       end
-      n += 1
     end
   end
   p array
 end
 
-def bubble_sort_by(array)
-   i=0
-  (array.length - 1).times do
-  val=yield( array[i], array[i+1])
-  puts val
-  if val<=0
-    i+=1
-  else 
-    array.insert(i + 1, array.delete_at(i))
-    i+=1
-  end
-end
-    p array
-end
-
-
 bubble_sort(unsorted_list)
 
+def bubble_sort_by(array)
+  k = 0
+  (array.length - 1).times do
+    k += 1
+    i = 0
+    (array.length - 1).times do
+      val = yield(array[i], array[i + 1])
+      array.insert(i + 1, array.delete_at(i)) if val.positive?
+      i += 1
+    end
+  end
+  p array
+end
 
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
